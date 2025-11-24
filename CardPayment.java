1f361f5f615ce738c4694b2f3032b8aaf952c886
firaa ramadhani <firarmd24@mhs.usk.ac.id>
2025-11-25 03:04:21 +0700

    Implement CardPayment class for credit/debit processing

---
+++ CardPayment.java
@@ -0,0 +1,18 @@
+package dumpir;
+
+import java.util.Scanner;
+
+// Implementasi pembayaran menggunakan Kartu Kredit/Debit
+public class CardPayment implements Pembayaran {
+
+    @Override
+    public boolean idPembayaran(double total, Scanner sc) {
+        System.out.println("Memproses Kartu Kredit/Debit sebesar Rp " + total);
+        return true;
+    }
+
+    @Override
+    public String getNamaMetode() { 
+        return "Card"; 
+    }
+}
